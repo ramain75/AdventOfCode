8f93ce51b929fc9a1e2b4823e49bfbcf7e83bfc5
Robert Main <ramain75@users.noreply.github.com>
2024-10-20 16:01:20 +0100

    Committing 2023 day 3, which hadn't been committed

--- src/main/java/com/adventofcode/year2023/day3/Day3.java
+++ src/main/java/com/adventofcode/year2023/day3/Day3.java
@@ -1,5 +1,6 @@
 package com.adventofcode.year2023.day3;
 
+import com.adventofcode.DayMode;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,11 +17,16 @@ import java.util.stream.Stream;
 public class Day3 {
     private static final Logger logger = LogManager.getLogger(Day3.class);
 
-    List<String> engineSchematic = new ArrayList<>();
+    private DayMode mode;
 
-    int partNumberTotal;
+    private List<String> engineSchematic = new ArrayList<>();
 
-    public Day3(String filename) {
+    private int partNumberTotal;
+
+    private int gearTotal;
+
+    public Day3(String filename, DayMode mode) {
+        this.mode = mode;
         processFile(filename);
     }
 
@@ -32,28 +38,128 @@ public class Day3 {
         }
 
         for (int i = 0; i < engineSchematic.size(); i++) {
-            findPartNumbersOnEngineLine(i);
+            findPartNumbersOnEngineLine(
+                    i>0 ? engineSchematic.get(i-1) : null,
+                    engineSchematic.get(i),
+                    i+1<engineSchematic.size() ? engineSchematic.get(i+1) : null,
+                    i);
+        }
+
+        for (int i = 0; i < engineSchematic.size(); i++) {
+            findGearsOnEngineLine(
+                    i>0 ? engineSchematic.get(i-1) : null,
+                    engineSchematic.get(i),
+                    i+1<engineSchematic.size() ? engineSchematic.get(i+1) : null,
+                    i);
         }
     }
 
-    private void findPartNumbersOnEngineLine(int lineNumber) {
+    private void findPartNumbersOnEngineLine(String prevEngineLine,
+                                             String currEngineLine,
+                                             String nextEngineLine,
+                                             int lineNumber) {
         Pattern p = Pattern.compile("\\d+");
-        Matcher m = p.matcher(engineSchematic.get(lineNumber));
+        Matcher m = p.matcher(currEngineLine);
 
         while(m.find()) {
             String digitStr = m.group();
             int startIndex = m.start();
-            int endIndex = m.end();
-            boolean part = isPartNumber(lineNumber, startIndex, endIndex);
+            int endIndex = m.end() - 1;
+            boolean part = isPartNumber(prevEngineLine, currEngineLine, nextEngineLine, digitStr, lineNumber, startIndex, endIndex);
             if (part) {
                 partNumberTotal += Integer.parseInt(digitStr);
             }
         }
     }
 
-    private boolean isPartNumber(int lineNumber, int startIndex, int endIndex) {
-        // TODO: Currently doesn't check the engine around the number
-        return true;
+    private void findGearsOnEngineLine(String prevEngineLine,
+                                             String currEngineLine,
+                                             String nextEngineLine,
+                                             int lineNumber) {
+        for (int i = 0; i < currEngineLine.length(); i++) {
+            if (currEngineLine.charAt(i) == '*') {
+                logger.debug("Found possible gear at index [{}]", i);
+            }
+        }
+    }
+
+    /**
+     * check the engine around the number for a symbol
+     * return: true on first match, false if no matches
+      */
+    private boolean isPartNumber(String prevEngineLine,
+                                 String currEngineLine,
+                                 String nextEngineLine,
+                                 String digitStr,
+                                 int lineNumber,
+                                 int startIndex,
+                                 int endIndex) {
+        boolean checkPrevLine = null != prevEngineLine;
+        boolean checkNextLine = null != nextEngineLine;
+
+        int startX = startIndex > 0 ? startIndex -1 : 0;
+        int endX = endIndex + 1 < currEngineLine.length() ? endIndex + 1 : endIndex;
+
+        if (checkPrevLine) {
+            for (int i = startX; i <= endX; i++) {
+                char c = prevEngineLine.charAt(i);
+                // Look for symbols (I'm defining as not a dot or a number)
+                if (c != '.' && (c < 48 || c > 57)) {
+                    logger.debug("Found match for {} on previous line, char {}", digitStr, i);
+                    return true;
+                }
+            }
+        }
+
+        if (startX < startIndex) {
+            char c = currEngineLine.charAt(startX);
+            if (c != '.' && (c < 48 || c > 57)) {
+                logger.debug("Found match for {} on left, char {}", digitStr, startX);
+                return true;
+            }
+        }
+
+        if (endX > endIndex) {
+            char c = currEngineLine.charAt(endX);
+            if (c != '.' && (c < 48 || c > 57)) {
+                logger.debug("Found match for {} on right, char {}", digitStr, endX);
+                return true;
+            }
+        }
+
+        if (checkNextLine) {
+            for (int i = startX; i <= endX; i++) {
+                char c = nextEngineLine.charAt(i);
+                if (c != '.' && (c < 48 || c > 57)) {
+                    logger.debug("Found match for {} on next line, char {}", digitStr, i);
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Is there a digit in more than one
+     * @param prevEngineLine
+     * @param currEngineLine
+     * @param nextEngineLine
+     * @param digitStr
+     * @param lineNumber
+     * @param startIndex
+     * @param endIndex
+     * @return If this is a gear, it will return a positive number. If not, it will return 0.
+     */
+    private int isGear(String prevEngineLine,
+                       String currEngineLine,
+                       String nextEngineLine,
+                       String digitStr,
+                       int lineNumber,
+                       int startIndex,
+                       int endIndex) {
+        // TODO: implement
+        return 0;
     }
 
     private void processLine(String line) {
@@ -63,5 +169,9 @@ public class Day3 {
 
     public int getPartNumberTotal() {
         return partNumberTotal;
+    }
+
+    public int getGearTotal() {
+        return gearTotal;
     }
 }
--- src/test/java/com/adventofcode/year2023/day3/Day3Test.java
+++ src/test/java/com/adventofcode/year2023/day3/Day3Test.java
@@ -1,5 +1,6 @@
 package com.adventofcode.year2023.day3;
 
+import com.adventofcode.DayMode;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,8 +9,25 @@ public class Day3Test {
     @Test
     void testDay3Part1Sample() {
         String filename = "src/main/resources/2023day3sample.txt";
-        Day3 day3 = new Day3(filename);
+        Day3 day3 = new Day3(filename, DayMode.PART1);
 
         assertEquals(4361, day3.getPartNumberTotal(), "Invalid part number total");
+    }
+
+    @Test
+    void testDay3Part2Sample() {
+        String filename = "src/main/resources/2023day3sample.txt";
+        Day3 day3 = new Day3(filename, DayMode.PART2);
+
+        assertEquals(467835, day3.getPartNumberTotal(), "Invalid part number total");
+    }
+
+    @Test
+    void testDay3Part1Input() {
+        String filename = "src/main/resources/2023day3input.txt";
+        Day3 day3 = new Day3(filename, DayMode.PART1);
+
+        // not 453418
+        assertEquals(520135, day3.getPartNumberTotal(), "Invalid part number total");
     }
 }
