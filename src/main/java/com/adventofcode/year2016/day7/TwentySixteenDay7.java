7625482a868b9d5bdc7de537090bea458c27b115
Robert Main <ramain75@users.noreply.github.com>
2024-10-25 14:53:03 +0100

    2016 day 7 part 2

--- src/main/java/com/adventofcode/year2016/day7/TwentySixteenDay7.java
+++ src/main/java/com/adventofcode/year2016/day7/TwentySixteenDay7.java
@@ -1,24 +1,36 @@
 package com.adventofcode.year2016.day7;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class TwentySixteenDay7 {
 
+    static Predicate<String> isABBA = s -> s.length() == 4
+            && s.charAt(0) == s.charAt(3) && s.charAt(1) == s.charAt(2)
+            && s.charAt(0) != s.charAt(1)
+            && s.charAt(0) >= 'a' && s.charAt(0) <= 'z'
+            && s.charAt(1) >= 'a' && s.charAt(1) <= 'z';
+
+    static Predicate<String> isABAorBAB = s -> s.length() == 3
+            && s.charAt(0) == s.charAt(2)
+            && s.charAt(0) != s.charAt(1)
+            && s.charAt(0) >= 'a' && s.charAt(0) <= 'z'
+            && s.charAt(1) >= 'a' && s.charAt(1) <= 'z';
+
+    static Function<String, String> reverse = s -> {
+        if (isABAorBAB.test(s)) {
+            return String.valueOf(s.charAt(1)) +
+                    s.charAt(0) +
+                    s.charAt(1);
+        }
+        throw new RuntimeException("Invalid ABA or BAB");
+    };
+
     // ABBA == Autonomous Bridge Bypass Annotation
     static Predicate<String> hasIpAbba = ip -> {
-        Predicate<String> isABBA = s -> s.length() == 4
-                && s.charAt(0) == s.charAt(3) && s.charAt(1) == s.charAt(2)
-                && s.charAt(0) != s.charAt(1)
-                && s.charAt(0) >= 'a' && s.charAt(0) <= 'z'
-                && s.charAt(1) >= 'a' && s.charAt(1) <= 'z'
-                && s.charAt(2) >= 'a' && s.charAt(2) <= 'z'
-                && s.charAt(3) >= 'a' && s.charAt(3) <= 'z';
-
         int currChar = -1;
         boolean inBrackets = false;
         boolean foundABBAOutsideBrackets = false;
@@ -47,5 +59,48 @@ public class TwentySixteenDay7 {
 
     static long countIPsSupportingTLS(String input) {
         return Arrays.stream(input.split(System.getProperty("line.separator"))).filter(hasIpAbba).count();
+    }
+
+    static Predicate<String> supportsSSL = ip -> {
+        List<String> areaBroadcastAccessors = new ArrayList<>();
+        List<String> byteAllocationBlocks = new ArrayList<>();
+
+        System.out.printf("Processing: %s\n", ip);
+
+        int currChar = -1;
+        boolean inBrackets = false;
+
+        while (ip.length() > currChar + 3) {
+            currChar++;
+
+            if (ip.charAt(currChar) == '[') {
+                inBrackets = true;
+            } else if (ip.charAt(currChar) == ']') {
+                inBrackets = false;
+            }
+
+            String possibleABAorBAB = ip.substring(currChar, currChar+3);
+            if (isABAorBAB.test(possibleABAorBAB)) {
+                if (inBrackets) {
+                    // We're cheating here by reversing before putting in , so these are technically ABAs
+                    byteAllocationBlocks.add(reverse.apply(possibleABAorBAB));
+                } else {
+                    areaBroadcastAccessors.add(possibleABAorBAB);
+                }
+            }
+        }
+
+        System.out.printf("Area Broadcast Accessors: %s\n", areaBroadcastAccessors.toString());
+        System.out.printf("Byte Allocation Blocks: %s\n", byteAllocationBlocks.toString());
+
+        areaBroadcastAccessors.retainAll(byteAllocationBlocks);
+
+        System.out.printf("Intersecting ABAs and BABs: %s\n", areaBroadcastAccessors.toString());
+
+        return !areaBroadcastAccessors.isEmpty();
+    };
+
+    static long countIPsSupportingSSL(String input) {
+        return Arrays.stream(input.split(System.getProperty("line.separator"))).filter(supportsSSL).count();
     }
 }
--- src/test/java/com/adventofcode/year2016/day7/TwentySixteenDay7Test.java
+++ src/test/java/com/adventofcode/year2016/day7/TwentySixteenDay7Test.java
@@ -39,4 +39,33 @@ public class TwentySixteenDay7Test {
         String puzzleInput = Files.readString(Path.of(this.getClass().getClassLoader().getResource("2016day7input.txt").getFile()));
         assertEquals(118, TwentySixteenDay7.countIPsSupportingTLS(puzzleInput), "Count of IPs supporting TLS is not as expected");
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"aba[bab]xyz", "aaa[kek]eke", "zazbz[bzb]cdb"})
+    void testSupportsSSLTrue(String input) {
+        assertTrue(TwentySixteenDay7.supportsSSL.test(input));
+    }
+
+    @Test
+    void testSupportsSSLFalse() {
+        assertFalse(TwentySixteenDay7.supportsSSL.test("xyx[xyx]xyx"));
+    }
+
+    @Test
+    void testcountIPsSupportingSSLSampleInput() {
+        String sampleInput = """
+            aba[bab]xyz
+            xyx[xyx]xyx
+            aaa[kek]eke
+            zazbz[bzb]cdb""";
+
+        assertEquals(3, TwentySixteenDay7.countIPsSupportingSSL(sampleInput), "Count of IPs supporting SSL is not as expected");
+    }
+
+    @Test
+    void testcountIPsSupportingSSLPuzzleInput() throws IOException {
+        String puzzleInput = Files.readString(Path.of(this.getClass().getClassLoader().getResource("2016day7input.txt").getFile()));
+        assertEquals(260, TwentySixteenDay7.countIPsSupportingSSL(puzzleInput), "Count of IPs supporting SSL is not as expected");
+    }
+
 }
