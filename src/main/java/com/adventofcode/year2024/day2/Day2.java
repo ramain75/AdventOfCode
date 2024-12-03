0f2a74b50ba92159cf36edc64f9cc15679c3ee55
Robert Main <ramain75@users.noreply.github.com>
2024-12-03 21:11:33 +0000

    Initial versions for day 2

---
+++ src/main/java/com/adventofcode/year2024/day2/Day2.java
@@ -0,0 +1,5 @@
+package com.adventofcode.year2024.day2;
+
+public class Day2 {
+
+}
---
+++ src/test/java/com/adventofcode/year2024/day2/Day2Test.java
@@ -0,0 +1,16 @@
+package com.adventofcode.year2024.day2;
+
+import org.junit.jupiter.api.Test;
+
+public class Day2Test {
+    @Test
+    void testGetNumSafeReports() {
+        String input = """
+                7 6 4 2 1
+                1 2 7 8 9
+                9 7 6 2 1
+                1 3 2 4 5
+                8 6 4 4 1
+                1 3 6 7 9""";
+    }
+}
