99c24e3624acb8b7f0b145af6758778be06f75aa
Robert Main <ramain75@users.noreply.github.com>
2024-12-04 10:06:38 +0000

    Completed day 2

--- src/main/java/com/adventofcode/year2024/day2/Day2.java
+++ src/main/java/com/adventofcode/year2024/day2/Day2.java
@@ -1,5 +1,60 @@
 package com.adventofcode.year2024.day2;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Day2 {
 
+    private boolean isSafeReport(int[] levels) {
+        boolean safeIncreasing = true;
+        boolean safeDecreasing = true;
+        for (int i = 1; i < levels.length; i++) {
+            // check that it increases by at least one and at most three
+            if (safeIncreasing && (levels[i-1] >= levels[i] || levels[i-1]+3 < levels[i])) {
+                safeIncreasing = false;
+                if (!safeDecreasing) {
+                    break;
+                }
+            }
+
+            // check that it decreases by at least one and at most three
+            if (safeDecreasing && (levels[i-1] <= levels[i] || levels[i-1]-3 > levels[i])) {
+                safeDecreasing = false;
+                if (!safeIncreasing) {
+                    break;
+                }
+            }
+        }
+
+        return safeDecreasing || safeIncreasing;
+    }
+
+    public int getNumSafeReports(String input, boolean applyDampener) {
+        int numOfSafeReports = 0;
+
+        outer: for (String s : input.split("\\r?\\n")) {
+            System.out.println(s);
+            int[] levels = Arrays.stream(s.trim().split("\\s")).mapToInt(Integer::parseInt).toArray();
+            if (isSafeReport(levels)) {
+                System.out.println("Matched without dampener: " + Arrays.toString(levels));
+                numOfSafeReports++;
+                continue outer;
+            }
+
+            if (applyDampener) {
+                for (int levelToSkip = 0; levelToSkip < levels.length; levelToSkip++) {
+                    int[] start = Arrays.copyOfRange(levels, 0, levelToSkip);
+                    int[] end = levelToSkip < levels.length - 1 ? Arrays.copyOfRange(levels, levelToSkip+1, levels.length) : new int[0];
+                    int[] combination = IntStream.concat(Arrays.stream(start), Arrays.stream(end)).toArray();
+                    if (isSafeReport(combination)) {
+                        System.out.println("Matched with dampener: " + Arrays.toString(levels) + ", matching combination: " + Arrays.toString(combination));
+                        numOfSafeReports++;
+                        continue outer;
+                    }
+                }
+            }
+        }
+
+        return numOfSafeReports;
+    }
 }
--- src/test/java/com/adventofcode/year2024/day2/Day2Test.java
+++ src/test/java/com/adventofcode/year2024/day2/Day2Test.java
@@ -1,10 +1,11 @@
 package com.adventofcode.year2024.day2;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Day2Test {
     @Test
-    void testGetNumSafeReports() {
+    void testGetNumSafeReportsSampleInput() {
         String input = """
                 7 6 4 2 1
                 1 2 7 8 9
@@ -12,5 +13,23 @@ public class Day2Test {
                 1 3 2 4 5
                 8 6 4 4 1
                 1 3 6 7 9""";
+
+        Day2 day2 = new Day2();
+        Assertions.assertEquals(2, day2.getNumSafeReports(input, false));
     }
+
+    @Test
+    void testGetNumSafeReportsWithDampenerSampleInput() {
+        String input = """
+                7 6 4 2 1
+                1 2 7 8 9
+                9 7 6 2 1
+                1 3 2 4 5
+                8 6 4 4 1
+                1 3 6 7 9""";
+
+        Day2 day2 = new Day2();
+        Assertions.assertEquals(4, day2.getNumSafeReports(input, true));
+    }
+
 }
